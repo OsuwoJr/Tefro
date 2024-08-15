ecdc10487941d123d23634683b74f76a3949739b
OsuwoJr <johnfredosuwo@gmail.com>
2024-08-15 16:41:10 +0300

    Hero component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
     
-  </script>
+</script>
+
+<style>
   
-  <style>
-    
-    .hero-text {
-      text-shadow: 0 0 10px #00ff0064; 
-    }
-  </style>
-  
-  <section class="flex flex-col items-center justify-center h-screen text-center text-white">
-    <h1 class="text-4xl md:text-6xl font-bold hero-text mb-4">
-      BUILD WITH WEB3 FRIENDS
-    </h1>
-    <p class="text-xl md:text-2xl mb-8">
-      Tefro's mission is to accelerate the education and impact of a new wave of web3 builders
-    </p>
-    <div class="flex space-x-4">
-        <div class="flex space-x-4">
-            <button class="bg-gray-800 text-white px-6 py-2 rounded-full shadow-md hover:bg-gray-700 transition-all duration-300 flex items-center space-x-2">
-              <span>Get Involved</span>
-              <span role="img" aria-label="Get Involved">🚀</span>
-            </button>
-            <button class="bg-transparent border border-emerald-500 text-white px-6 py-2 rounded-full shadow-md hover:border-emerald-100 hover:bg-emerald-800 transition-all duration-300 flex items-center space-x-2">
-              <span>Partner with Us</span>
-              <span role="img" aria-label="Partner with Us">🤝</span>
-            </button>
-          </div>
-  </section>
-  
+  .hero-text {
+    text-shadow: 0 0 10px #00ff0064; 
+  }
+</style>
+
+<section class="flex flex-col items-center justify-center h-screen text-center text-white">
+  <h1 class="text-4xl md:text-6xl font-bold hero-text mb-4">
+    BUILD WITH WEB3 FRIENDS
+  </h1>
+  <p class="text-xl md:text-2xl mb-8">
+    Tefro's mission is to accelerate the education and impact of a new wave of web3 builders
+  </p>
+  <div class="flex space-x-4">
+      <div class="flex space-x-4">
+          <button class="bg-gray-800 text-white px-6 py-2 rounded-full shadow-md hover:bg-gray-700 transition-all duration-300 flex items-center space-x-2">
+            <span>Get Involved</span>
+            <span role="img" aria-label="Get Involved">🚀</span>
+          </button>
+          <button class="bg-transparent border border-emerald-500 text-white px-6 py-2 rounded-full shadow-md hover:border-emerald-100 hover:bg-emerald-800 transition-all duration-300 flex items-center space-x-2">
+            <span>Partner with Us</span>
+            <span role="img" aria-label="Partner with Us">🤝</span>
+          </button>
+        </div>
+</section>
