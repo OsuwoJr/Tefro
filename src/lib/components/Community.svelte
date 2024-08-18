3163b43df823f312b881d3ce1fa6e25f6a1c594a
OsuwoJr <johnfredosuwo@gmail.com>
2024-08-18 20:25:23 +0300

    Hero spacing

--- src/lib/components/Community.svelte
+++ src/lib/components/Community.svelte
@@ -14,7 +14,7 @@
     }
   </style>
   
-  <section class="flex flex-col items-center justify-center text-center text-white py-12 px-6">
+  <section class="flex flex-col items-center justify-center  text-center text-white py-12 px-6">
     <p class="text-sm text-gray-300 mb-2">We are...</p>
     <h2 class="text-4xl md:text-5xl font-bold hero-text mb-4 uppercase leading-snug">
       Developer owned &<br />governed community
--- src/lib/components/Fame.svelte
+++ src/lib/components/Fame.svelte
@@ -63,7 +63,7 @@
     ];
   </script>
   
-  <section class="bg-emerald-900 text-white py-12 px-6">
+  <section class="bg-emerald-900 text-white py-12  px-6">
     <div class="container mx-auto">
       <h2 class="text-3xl font-bold text-center mb-8 hero-text">Hall Of Fame</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
